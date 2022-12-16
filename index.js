633fb67965c1946af436a886a9933dca38b83e53
Dale McConnell <dale.mcconnell@wundermanthompson.com>
2022-12-16 15:58:28 +0100

    Add more deployment logging

--- index.js
+++ index.js
@@ -20,5 +20,6 @@ app.get('/', function (req, res) {
  
 app.listen(PORT, function () {
   console.log(`App listening on port ${PORT}`);
+  console.log('Express is a: '+typeof express);
 });
 // Comment for deployment 
