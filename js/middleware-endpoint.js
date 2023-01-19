189e4acf30e836b2e7560b6b0ad6b7a9b02fd05e
Dale McConnell <dale.mcconnell@wundermanthompson.com>
2023-01-19 12:07:51 +0100

    Updated middleware snippet

--- js/middleware-endpoint.js
+++ js/middleware-endpoint.js
@@ -1,26 +1,24 @@
+import axios from "axios"
 export default defineComponent({
-    async run({ steps, $ }) {
-      const headers = {}
-  
-      const bodyContent = {
-        "pushNotificationText":steps.trigger.event.body.inArguments[0].message
-      }
-      const url = steps.trigger.event.body.inArguments[0].endpoint
-      
-      await $.respond({
-        status: 200,
-        headers,
-        bodyContent
-      }).then(
-      fetch(url, {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
+  async run({ steps, $ }) {
+    const headers = {}
+
+    const bodyContent = {
+      "pushNotificationText":steps.trigger.event.inArguments[0].message
+    }
+    const externalUrl = steps.trigger.event.inArguments[0].endpoint
+    
+    await $.respond({
+      status: 200,
+      headers,
+      bodyContent
+    }).then(async function(){
+      const { data } = await axios({
+        url: externalUrl,
+        headers: headers,
         body: bodyContent
-    }).then(response => response.json())
-    .then(response => console.log(JSON.stringify(response)))
-      )
-    },
-  })
+      })
+      return data
+    })
+  },
+})
