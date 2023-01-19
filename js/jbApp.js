9f42eee17facfd11672b786baf026d10f2cb6546
Dale McConnell <dale.mcconnell@wundermanthompson.com>
2023-01-19 13:07:13 +0100

    Remove passId param from RequestBin url

--- js/jbApp.js
+++ js/jbApp.js
@@ -16,7 +16,7 @@ const jbApp = {
     passId:null,
     credentials:{
         dev:{
-            'url': 'https://eol3vy07fc9qzyh.m.pipedream.net/?passId={passId}',
+            'url': 'https://eol3vy07fc9qzyh.m.pipedream.net/',
             'auth': '8cn/SZm168HpBz_dUK&GvEIxwL6xbf8YE8rB3Il9tO_od0XngAeBV9tLe_LykQxPC4A4i0K1zKoOlxQ0'
         },
         prod:{
